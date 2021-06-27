de135d877e614db3cf825802ae8c86a8f9aad20f
Theseregrets <rishitashaw49@gmail.com>
2021-06-28 01:18:08 +0530

    texture loaded

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,16 +1,16 @@
 uniform float time;
 uniform float progress;
-uniform sampler20 texture;
+uniform sampler2D texture;
 uniform vec4 resolution;
 varying vec2 vUv;
 varying vec3 vPosition;
 varying vec3 vNormal;
-float PI = 3.1415926535979
+float PI = 3.1415926535979;
 
 void main(){
-    float diff = dot(vec3(1.),vNormal)
-    gl_FragColor = vec4(vUv, 1.0);
+    float diff = dot(vec3(1.),vNormal);
+    gl_FragColor = vec4(vUv,0., 1.);
     gl_FragColor = vec4(vNormal, 1.0);
-    gl_FragColor = vec4(abs(sin(diff*10.));
+    gl_FragColor = vec4(abs(sin(diff* 10.)));
     
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -4,12 +4,12 @@ varying vec2 vUv;
 varying vec3 vNormal;
 uniform vec2 pixels;
 
-float PI = 3.1415926535979
+float PI = 3.1415926535979;
 
 
 void main() {
     vUv=uv;
-    vNormal=normal
+    vNormal=normal;
 
     vec4 mvPosition = modelViewMatrix * vec4 (position, 1.);
 
